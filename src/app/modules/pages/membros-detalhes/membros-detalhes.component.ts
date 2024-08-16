1cfbdf06e817c7619c308a8c798ecd2abca02852
Adryano Chavier <adryano.chavieroficial@gmail.com>
2024-08-16 20:41:43 -0300

    Reajustando problema no json

--- src/app/modules/pages/membros-detalhes/membros-detalhes.component.ts
+++ src/app/modules/pages/membros-detalhes/membros-detalhes.component.ts
@@ -3,11 +3,10 @@ import { ActivatedRoute } from '@angular/router';
 import { MembroService } from './../../services/membro.service';
 import { Component, OnInit, inject} from '@angular/core';
 import { TabsModule } from 'ngx-bootstrap/tabs';
-import { GalleryItem, GalleryModule, ImageItem } from 'ng-gallery';
 @Component({
   selector: 'app-membros-detalhes',
   standalone: true,
-  imports: [TabsModule,GalleryModule],
+  imports: [TabsModule],
   templateUrl: './membros-detalhes.component.html',
   styleUrl: './membros-detalhes.component.css'
 })
@@ -15,7 +14,7 @@ export class MembrosDetalhesComponent implements OnInit{
 private membroService = inject(MembroService);
 private route = inject(ActivatedRoute)
 membro?: Membro;
-imagens: GalleryItem[]=[]
+
 
 ngOnInit(): void {
 this.loadMembro()
@@ -25,12 +24,8 @@ loadMembro(){
   const usuario_nome = this.route.snapshot.paramMap.get('usuario_nome');
   if(!usuario_nome) return;
   this.membroService.getMembro(usuario_nome).subscribe({
-    next: membro => {
-      this.membro = membro;
-      membro.fotos.map(p =>{
-        this.imagens.push(new ImageItem({src: p.url, thumb: p.url}))
-      })
-    }
+    next: membro => this.membro = membro
+
   })
 }
 
