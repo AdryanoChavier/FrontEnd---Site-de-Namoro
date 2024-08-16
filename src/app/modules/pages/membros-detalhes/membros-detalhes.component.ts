e07b9254a342c6f076b4d49c13039feb280effa6
Adryano Chavier <adryano.chavieroficial@gmail.com>
2024-08-16 20:15:11 -0300

    Adicionado  alguma partes do perfil do membro

--- src/app/modules/pages/membros-detalhes/membros-detalhes.component.ts
+++ src/app/modules/pages/membros-detalhes/membros-detalhes.component.ts
@@ -2,11 +2,12 @@ import { Membro } from './../../interface/membro';
 import { ActivatedRoute } from '@angular/router';
 import { MembroService } from './../../services/membro.service';
 import { Component, OnInit, inject} from '@angular/core';
-
+import { TabsModule } from 'ngx-bootstrap/tabs';
+import { GalleryItem, GalleryModule, ImageItem } from 'ng-gallery';
 @Component({
   selector: 'app-membros-detalhes',
   standalone: true,
-  imports: [],
+  imports: [TabsModule,GalleryModule],
   templateUrl: './membros-detalhes.component.html',
   styleUrl: './membros-detalhes.component.css'
 })
@@ -14,6 +15,7 @@ export class MembrosDetalhesComponent implements OnInit{
 private membroService = inject(MembroService);
 private route = inject(ActivatedRoute)
 membro?: Membro;
+imagens: GalleryItem[]=[]
 
 ngOnInit(): void {
 this.loadMembro()
@@ -23,7 +25,12 @@ loadMembro(){
   const usuario_nome = this.route.snapshot.paramMap.get('usuario_nome');
   if(!usuario_nome) return;
   this.membroService.getMembro(usuario_nome).subscribe({
-    next: membro => this.membro = membro
+    next: membro => {
+      this.membro = membro;
+      membro.fotos.map(p =>{
+        this.imagens.push(new ImageItem({src: p.url, thumb: p.url}))
+      })
+    }
   })
 }
 
