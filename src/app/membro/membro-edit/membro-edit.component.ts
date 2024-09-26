03afaaccdde56fd20d5698a3bc3ed89bf4c72e9e
adryano.chavier <adryano.chavier@senac.br>
2024-09-26 11:01:59 -0300

    Aula 110

--- src/app/membro/membro-edit/membro-edit.component.ts
+++ src/app/membro/membro-edit/membro-edit.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Component, OnInit, inject } from '@angular/core';
+import { Membro } from '../../modules/interface/membro';
+import { ContaService } from '../../modules/services/conta.service';
+import { MembroService } from '../../modules/services/membro.service';
+import { TabsModule } from 'ngx-bootstrap/tabs';
 
 @Component({
   selector: 'app-membro-edit',
   standalone: true,
-  imports: [],
+  imports: [TabsModule,FormsModule],
   templateUrl: './membro-edit.component.html',
   styleUrl: './membro-edit.component.css'
 })
-export class MembroEditComponent {
+export class MembroEditComponent implements OnInit{
+  membro?: Membro;
+  private contaService = inject(ContaService);
+  private membroService = inject(MembroService);
 
+  ngOnInit(): void {
+    this.carregarMembro()
+
+  }
+  carregarMembro(){
+    const usuario = this.contaService.usuarioAtual()
+    if(!usuario) return;
+    this.membroService.getMembro(usuario.usuario_nome).subscribe({
+      next: membro => this.membro = membro
+    })
+  }
 }
+
